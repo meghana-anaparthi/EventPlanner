3238384af549b15cc9e885d0b8d1d200d2057475
deekshita98 <dsavanur@gmail.com>
2018-02-18 18:56:08 +0530

    Created Splash Screen Activity

---
+++ app/src/main/java/com/example/ameghana/eventplanner/SplashScreenActivity.java
@@ -0,0 +1,37 @@
+package com.example.ameghana.eventplanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ImageView imageView= (ImageView) findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
+        imageView.startAnimation(animation);
+
+        Thread timer = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
+                    finish();
+                    super.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        timer.start();
+    }
+}
